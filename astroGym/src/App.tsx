20b82c14c9209042de6eeeb5b3bd49dd79babeba
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-01-24 15:07:09 +0000

    feat: setup files for the project

---
+++ astroGym/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app bg-slate-400">
+      <h1 className=" flex h-screen items-center justify-center border-2 text-lg font-bold text-red-600">
+        Hello Astro
+      </h1>
+    </div>
+  );
+}
+
+export default App;
