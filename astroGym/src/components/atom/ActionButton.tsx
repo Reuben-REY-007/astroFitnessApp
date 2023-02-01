14dcac13247612442ae252ce45cdd0235934c1ef
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-01 12:14:16 +0000

    refactor: change the css style

--- astroGym/src/components/atom/ActionButton.tsx
+++ astroGym/src/components/atom/ActionButton.tsx
@@ -5,7 +5,7 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 const ActionButton = ({ children, setSelectedPage }: ActionButtonProps) => {
   return (
     <AnchorLink
-      className=" rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500"
+      className=" rounded-md bg-secondary-500 px-8 py-2 hover:bg-primary-500"
       onClick={() => setSelectedPage(SelectedPage.ContactUs)}
       href={`#${SelectedPage.ContactUs}`}
     >
