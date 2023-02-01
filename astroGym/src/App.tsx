735cd2d64bdc825d2fe0ef98ccad400528ffa534
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-01 08:42:23 +0000

    feat: add function for navbar

--- astroGym/src/App.tsx
+++ astroGym/src/App.tsx
@@ -1,9 +1,30 @@
+import Navbar from "@/components/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/components/types/types";
+import Homepage from "@/components/pages/homepage";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setisTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setisTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setisTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="app bg-slate-400">
-      <h1 className=" flex h-screen items-center justify-center border-2 text-lg font-bold text-red-600">
-        Hello Astro
-      </h1>
+    <div className="app bg-gray-20">
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Homepage setSelectedPage={setSelectedPage}/>
     </div>
   );
 }
