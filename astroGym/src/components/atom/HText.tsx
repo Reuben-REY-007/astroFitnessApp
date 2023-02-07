57f6c3f5915811f4fd169e021a7ab182db5c0043
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-07 09:48:48 +0000

    feat: add Text component

---
+++ astroGym/src/components/atom/HText.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HTextProps } from '../types/types'
+
+const HText = ({children}: HTextProps) => {
+  return (
+    <h1 className=" basis-3/5 font-montserrat text-3xl font-bold">
+        {children}
+    </h1>
+  )
+}
+
+export default HText
