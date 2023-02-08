24bafe78c423f18b11491558935491d4d01aff0c
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-08 10:45:12 +0000

    feat: import new components

--- astroGym/src/App.tsx
+++ astroGym/src/App.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/components/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/components/types/types";
-import Homepage from "@/components/pages/homepage";
+import Homepage from "@/components/pages/homepage/home";
+import Benefits from "./components/pages/homepage/benefits";
+import OurClasses from "./components/pages/homepage/ourClasses";
+import ContactUs from "./components/pages/homepage/contactUs";
+import Footer from "./components/pages/homepage/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -25,6 +29,10 @@ function App() {
     <div className="app bg-gray-20">
       <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       <Homepage setSelectedPage={setSelectedPage}/>
+      <Benefits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
