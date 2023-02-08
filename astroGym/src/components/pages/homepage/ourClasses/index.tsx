e4b02d3dc97f22de750cded9d9d08ec65fbd084d
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-08 10:44:07 +0000

    feat: add classes, data and description components

---
+++ astroGym/src/components/pages/homepage/ourClasses/index.tsx
@@ -0,0 +1,55 @@
+import HText from "@/components/atom/HText";
+import {
+  ClassProps,
+  OurClassesProps,
+  SelectedPage,
+} from "@/components/types/types";
+import { motion } from "framer-motion";
+import { classes } from "./data/ArrayData";
+import Class from "./Class";
+
+const OurClasses = ({ setSelectedPage }: OurClassesProps) => {
+  return (
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+              tellus quam porttitor. Mauris velit euismod elementum arcu neque
+              facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+              enim mattis odio in risus nunc.
+            </p>
+          </div>
+        </motion.div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item: ClassProps, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default OurClasses;
