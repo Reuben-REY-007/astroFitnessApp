feb7e4b1770e628349927a187c42756452b82fcf
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-02-14 15:42:32 +0000

    refactor: change to routes

--- astroGym/src/App.tsx
+++ astroGym/src/App.tsx
@@ -1,39 +1,15 @@
-import Navbar from "@/components/navbar";
-import { useEffect, useState } from "react";
-import { SelectedPage } from "@/components/types/types";
-import Homepage from "@/components/pages/homepage/home";
-import Benefits from "./components/pages/homepage/benefits";
-import OurClasses from "./components/pages/homepage/ourClasses";
-import ContactUs from "./components/pages/homepage/contactUs";
-import Footer from "./components/pages/homepage/footer";
-
+import { Route, Routes } from "react-router-dom";
+import Landingpage from "./components/Landingpage";
+import AccountPage from "./components/pages/accounts/AccountPage";
+import Signup from "./components/pages/accounts/Signup";
+import { SetStateAction } from "react";
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
-    SelectedPage.Home
-  );
-  const [isTopOfPage, setisTopOfPage] = useState<boolean>(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setisTopOfPage(true);
-        setSelectedPage(SelectedPage.Home);
-      }
-      if (window.scrollY !== 0) setisTopOfPage(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
+  
   return (
-    <div className="app bg-gray-20">
-      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-      <Homepage setSelectedPage={setSelectedPage}/>
-      <Benefits setSelectedPage={setSelectedPage}/>
-      <OurClasses setSelectedPage={setSelectedPage}/>
-      <ContactUs setSelectedPage={setSelectedPage} />
-      <Footer />
-    </div>
+    <Routes >
+      <Route path="/"  element={<Landingpage />} />
+      <Route path="/account"  element={<AccountPage />} />
+    </Routes>
   );
 }
 
