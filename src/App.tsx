2e238eda033852911e06899b5455b7aa1d42dd46
Reuben-REY-007 <reubenekowyawson@gmail.com>
2025-03-19 08:47:05 +0000

    Fix: Update React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Landingpage from "./components/Landingpage";
 import AccountPage from "./components/pages/accounts/AccountPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landingpage />} />
-        <Route path="/account" element={<AccountPage />} />
-      </Routes>
-    </Router>
+    <Routes >
+      <Route path="/"  element={<Landingpage />} />
+      <Route path="/account"  element={<AccountPage />} />
+    </Routes>
   );
 }
 
